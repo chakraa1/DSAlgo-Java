94a35399e0a8f6c8838dbbc0e85f7db83fb544a7
anindyachakrabortty@gmail.com <anindyachakrabortty@gmail.com>
2021-06-26 13:50:06 +0530

    additional login

--- src/com/dsalgo/binarysearch/RotatedSortedArraySearch.java
+++ src/com/dsalgo/binarysearch/RotatedSortedArraySearch.java
@@ -22,13 +22,44 @@ public class RotatedSortedArraySearch {
 		return searchedIndex;
 
 	}
-	
+	private int findPivot(int arr[], int low, int high){
+        // base cases
+        if (high < low)
+            return -1;
+        if (high == low)
+            return low;
+ 
+        /* low + (high - low)/2; */
+        int mid = (low + high) / 2;
+        // Corner cases (if mid is the pivot) start 
+        if (mid < high && arr[mid] > arr[mid + 1])
+            return mid;
+        if (mid > low && arr[mid] < arr[mid - 1])
+            return (mid - 1);
+        // Corner cases (if mid is the pivot) End  
+        if (arr[low] >= arr[mid])
+            return findPivot(arr, low, mid - 1);
+        return findPivot(arr, mid + 1, high);
+    }
 	private int rotatedSortedArrayBinarySearch(int[] inputArray, int lowIndex, int highIndex, int searchedKey) {
-		int searchedIndex = -1;
-
-		searchedIndex=binarySearch(inputArray,lowIndex,highIndex,searchedKey);
-
-		return searchedIndex;
+		//int searchedIndex = -1;
+        
+		int pivot = findPivot(inputArray, 0, highIndex);
+		 
+        // If we didn't find a pivot, then
+        // array is not rotated at all
+        if (pivot == -1)
+            return binarySearch(inputArray, 0, highIndex, searchedKey);
+ 
+        // If we found a pivot, then first
+        // compare with pivot and then
+        // search in two subarrays around pivot
+        if (inputArray[pivot] == searchedKey)
+            return pivot;
+        if (inputArray[0] <= searchedKey)
+            return binarySearch(inputArray, 0, pivot - 1, searchedKey);
+        return binarySearch(inputArray, pivot + 1, highIndex, searchedKey);
+        
 
 	}
 
@@ -38,7 +69,7 @@ public class RotatedSortedArraySearch {
 //		int[] A = { 4, 5, 6, 7, 0, 1, 2, 3 };
 //		int B = 5;
 		int A[] = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
-		int B = 6;
+		int B = 2;
 		int low=0, high=A.length -1;
 		System.out.println("Input Array --> " + Arrays.toString(A) +" searched key --> "+B);
 		
